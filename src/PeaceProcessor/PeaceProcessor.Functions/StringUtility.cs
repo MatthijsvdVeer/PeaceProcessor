d3c3d5ee37850627e647fe9d6da5a4261fb4b02b
Matthijs van der Veer <736727+MatthijsvdVeer@users.noreply.github.com>
2023-09-05 09:11:30 +0200

    Improving prompts (#19)
    * Improving prompts
    
    * Improving image prompt
    
    * Improving image prompt
    
    * Increasing metadata value length
    
    ---------
    
    Co-authored-by: Matthijs van der Veer <mvanderveer@xpirit.com>

--- src/PeaceProcessor/PeaceProcessor.Functions/StringUtility.cs
+++ src/PeaceProcessor/PeaceProcessor.Functions/StringUtility.cs
@@ -4,11 +4,11 @@
     {
         /// <summary>
         /// Blob storage metadata does not support newlines or non-ASCII characters. This method
-        /// removes not supported characters and trims the topic to 50 characters.
+        /// removes not supported characters and trims the topic to 1000 characters.
         /// </summary>
         public static string FormatForTopicMetadata(string topic)
         {
-            return new string(topic[..Math.Min(topic.Length, 50)]
+            return new string(topic[..Math.Min(topic.Length, 1000)]
                 .Replace("\n", "").Where(c => c < 128).ToArray());
         }
     }
