22a7458dcce1d0aa1943bd6b2e9afe89ed97cd09
Matthijs van der Veer <736727+MatthijsvdVeer@users.noreply.github.com>
2024-05-19 12:02:32 -0700

    Basic CLI setup

---
+++ src/PeaceProcessor/PeaceProcessor.CLI/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace PeaceProcessor.CLI;
+
+using System.CommandLine;
+
+class Program
+{
+    static async Task<int> Main(string[] args)
+    {
+        var rootCommand = new RootCommand("PeaceProcessor commands");
+        
+        var topicOption = new Option<string>(
+            aliases: ["--topic", "-t"],
+            description: "The topic for which to create a script for.");
+        var scriptCommand = new Command("script", "Create a script.");
+        scriptCommand.AddOption(topicOption);
+        
+        scriptCommand.SetHandler((topic) => 
+            { 
+                Console.WriteLine($"Creating script for topic: {topic}"); 
+            },
+            topicOption);
+
+        rootCommand.AddCommand(scriptCommand);
+        return await rootCommand.InvokeAsync(args);
+    }
+}
